b4a7f8502156c2d2ac8720bbfa154da99b441bcc
Deryn Boscariol <derynb@rogers.com>
2024-02-06 15:30:31 -0500

    adding week 4 files

---
+++ Week 4/JS-LAB-4-Functions-24w/lab-4-2.js
@@ -0,0 +1,30 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+function avgMark(mark1, mark2, mark3, mark4, mark5){
+    var average = (mark1 + mark2 + mark3 + mark4 + mark5) / 5;
+    return average.toFixed(1);
+
+}
+
+console.log ("Average mark is: " + avgMark(5, 10, 15, 20, 25));
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+var course1 = 80;
+var course2 = 85;
+var course3 = 60;
+var course4 = 79;
+var course5 = 66;
+
+var myAvg = avgMark(course1, course2, course3, course4, course5);
+
+if (myAvg >= 70){
+    alert("Congrats! Keep up the good work!");
+}
+else {
+    alert("Review required");
+}
---
+++ Week 4/JS-LAB-4-Functions-24w/lab-4-3.js
@@ -0,0 +1,34 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  WALKING THE DOG 
+
+
+//################## CREATE YOUR checkTemp FUNCTION
+//This function will determine whether or not it is above 30 or below -10
+//It expects to receive currentTemp as a number
+//It will return true or false
+
+function checkTemp (currentTemp){
+    if(currentTemp > 30){
+        return false;
+    }
+
+    if(currentTemp < -10){
+        return false;
+    }
+
+    else {
+        return true;
+    }
+}
+
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+
+userTemp = prompt("What is the current temperature?")
+
+if (checkTemp (userTemp) === false){
+    alert("Yikes! This is no weather for dog walking!");
+}
+else {
+    alert("You're good, have a nice walk!");
+}
