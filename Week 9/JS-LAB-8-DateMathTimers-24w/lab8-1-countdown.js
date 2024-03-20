03a33376cfee397305385f7959dbe4cd2500d109
Deryn Boscariol <derynb@rogers.com>
2024-03-20 15:33:46 -0400

    adding week 7 and 9 files

---
+++ Week 9/JS-LAB-8-DateMathTimers-24w/lab8-1-countdown.js
@@ -0,0 +1,35 @@
+﻿/* LAB 8-1 - FINAL COUNTDOWN!! */
+
+
+//create page load listener
+window.onload = pageLoaded;
+//create page load function
+function pageLoaded(){
+
+	//create variables for required HTML elements
+	var todayDateOutput = document.getElementById("todayData");
+	var dueDateOutput = document.getElementById("finalData");
+	var daysTillDueOutput = document.getElementById("dueData");
+	var passedMessage = document.getElementById("countMsg");
+	//create variables for now date and due date
+	var dateStamp = new Date();
+	var todayDate = dateStamp.toDateString();
+	var dueDateStamp = new Date("April 19, 2023 18:00");
+	var dueDate = dueDateStamp.toDateString();
+	//OUTPUT NOW DATE & DUE DATE TO HTML PAGE
+	todayDateOutput.innerHTML = todayDate;
+	dueDateOutput.innerHTML = dueDate;
+	//CONVERT TO UTC AND SUBTRACT TO GET TIME DIFFERENCE
+	var todayDateUTC = dateStamp.getTime();
+	var dueDateUTC = dueDateStamp.getTime();
+	var diffUTC = dueDateUTC - todayDateUTC;
+	//CONVERT TIME DIFFERENCE TO WHOLE NUMBER OF DAYS
+	var daysTillDue = diffUTC/86400000;
+	var fullDaysTillDue = Math.floor(daysTillDue);
+	daysTillDueOutput.innerHTML = fullDaysTillDue;
+	console.log(daysTillDue);
+	//LOGIC TO CHECK IF DUE DATE HAS PASSED, AND OUPUT APPROPRIATE MESSAGE TO HTML PAGE
+	if (daysTillDue <= 0){
+		passedMessage.innerHTML = "The deadline for the final assignment has passed!";
+	}
+}
---
+++ Week 9/JS-LAB-8-DateMathTimers-24w/lab8-2-clock.js
@@ -0,0 +1,41 @@
+﻿/* LAB 8.2 - STOP TIME */
+
+
+//create page load listener
+window.onload = pageLoaded;
+//create page load function
+function pageLoaded(){
+	//create variables for required HTML elements
+	var hoursDisplay = document.getElementById("hoursOut");
+	var minsDisplay = document.getElementById("minsOut");
+	var secsDisplay = document.getElementById("secsOut");
+	var startBtn = document.getElementById("btnStart");
+	var stopBtn = document.getElementById("btnStop");
+	//create time variable so all functions have access to it
+	var updateDisplayTime;
+	
+	//CREATE FUNCTION THAT DISPLAYS THE TIME
+	function displayTime(){
+		var currentTime = new Date();
+		var currentHours = currentTime.getHours();
+		var currentMins = currentTime.getMinutes();
+		var currentSecs = currentTime.getSeconds();
+		hoursDisplay.innerHTML = currentHours.toString().padStart(2,"0") + ":";
+		minsDisplay.innerHTML = currentMins.toString().padStart(2,"0") + ":";
+		secsDisplay.innerHTML = currentSecs.toString().padStart(2,"0");
+	}
+	
+	//CREATE FUNCTION TO START THE CLOCK.
+	function startClock(){
+		updateDisplayTime  = setInterval(displayTime, 1000);
+	}
+	
+	//CREATE FUNCTION TO STOP THE CLOCK
+	function stopClock(){
+		clearInterval(updateDisplayTime);
+	}
+	
+	// SET EVENT LISTENERS
+	startBtn.onclick = startClock;
+	stopBtn.onclick = stopClock;
+}
